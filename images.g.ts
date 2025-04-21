34115205ec5a3158f9df1eabd80710c7c6e453c2
Richard Knoll <riknoll@users.noreply.github.com>
2025-04-21 10:50:28 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "green_duck":return img`
+. . . . . . . . . . 8 7 8 . . . 
+. . . . . . . . . 8 7 8 . . . . 
+. . . . . . . . . 8 c . . . . . 
+. . . . . . 8 8 8 8 8 8 . . . . 
+. . . . . 8 8 7 7 7 7 7 8 . . . 
+. . . . 8 8 7 6 1 f 7 7 6 f . . 
+. . . . 8 7 7 1 f f 7 6 4 c . . 
+. . . . 8 7 7 6 f 8 6 6 4 4 . . 
+8 6 6 6 8 8 6 7 7 7 4 4 4 4 4 8 
+8 8 6 7 7 7 8 7 7 4 4 4 4 4 8 . 
+8 6 c 7 7 7 7 6 7 7 7 7 7 8 . . 
+c 6 6 c 6 7 7 8 7 7 7 7 7 7 8 . 
+c 8 6 6 c c 8 7 7 7 7 7 7 7 8 . 
+. c 6 6 6 6 6 6 7 7 7 7 7 6 8 . 
+. . c 8 6 6 6 6 6 7 7 7 8 8 . . 
+. . . c c c c c c c c 8 8 . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
